035deabee6c29f73bd640510953b1076f74ebd0c
CIT 230 <elz20002@byui.edu>
2022-04-30 12:24:33 -0700

    teamwork

--- src/js/productData.js
+++ src/js/productData.js
@@ -5,21 +5,18 @@ function convertToJson(res) {
       throw new Error("Bad Response");
     }
   }
-export default class ProductData  {
-    
+  export default class ProductData  {
     constructor(category) {
-        this.products = [];
-        this.category = category;
-        this.path = `../json/${this.category}.json`;
+      this.category = category;
+      this.path = `../json/${this.category}.json`;
     }
-    getProductsData() {
-        fetch("../json/tents.json")
-        .then(convertToJson)
-        .then((data) => return data);
+    getData() {
+      return fetch(this.path)
+        .then(convertToJson).then((data) => data);
     }
-    lookUpProduct(e) {
-        const product = this.products.find((item) => item.Id === e.target.dataset.id);
-        return product
+    async findProductById(id) {
+      const products = await this.getData()
+      return products.find((item) => item.Id === id);
     }
   }
 
