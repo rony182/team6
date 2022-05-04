22efb3413bb9ebe8630e59b9a26c6fa2d9b1cc96
jmattgiroux <jmattgiroux@gmail.com>
2022-05-03 23:01:48 -0600

    started using teacher's solution. Also changed cart link in header snippet to fix issue when clicking on cart link within cart

--- src/js/utils.js
+++ src/js/utils.js
@@ -64,3 +64,11 @@ export async function loadHeaderFooter() {
 
 
 }
+
+export function renderListWithTemplate(template, parent, list, callback){
+  list.forEach(item => {
+    const clone = template.content.cloneNode(true);
+    const templateWithData = callback(clone, item);
+    parent.appendChild(templateWithData);
+  })
+}
