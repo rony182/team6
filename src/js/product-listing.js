5e11ccbfe5445c4fb48100720a3a927c7bde46a9
jmattgiroux <jmattgiroux@gmail.com>
2022-05-07 18:21:13 -0600

    product-listing now uses the product-listing.js instead of main.js

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,1 +1,11 @@
 // Product Listing JS File
+
+import ProductData from ".//productData.js";
+import ProductList from "./productList.js";
+
+const dataSource = new ProductData("tents");
+
+const productListElement = document.querySelector(".product-list");
+
+const tentList = new ProductList("tents", dataSource, productListElement);
+tentList.init();
